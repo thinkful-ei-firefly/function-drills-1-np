32c231c991c5c1311484931c06375f9062afa91f
Nicole Pasquale <NSquale212@gmail.com>
2019-07-08 15:23:20 -0500

    Third Commit

--- functions.js
+++ functions.js
@@ -1,3 +1,7 @@
 'use strict';
 
-console.log('Hi, my name is Nicole and I\'m 30 years old');
+function createGreeting() {
+  console.log('Hi, my name is Nicole and I\'m 30 years old');
+}
+
+createGreeting();

ebc7045bc5ed9e37ef4b1e29b468d2f4779c25a9
Nicole Pasquale <NSquale212@gmail.com>
2019-07-08 16:33:37 -0500

    Eighth Commit

--- functions.js
+++ functions.js
@@ -1,12 +1,23 @@
 'use strict';
 
 function createGreeting(name, age) {
-  const yob = getYearOfBirth();
-  return `Hi, my name is ${name} and I'm ${age} years old. I was born in ${yob}.`;
+  let yob;
+  try {
+    yob = getYearOfBirth(age);
+  } 
+  catch (e) {
+    console.log(e.message);
+    return;
+  }
+    return `Hi, my name is ${name} and I'm ${age} years old. I was born in ${yob}.`;
 }
 
 function getYearOfBirth(age) {
-  return 2019 - age;
+  if (age < 0) {
+    throw new Error('Age cannot be negative.');
+  } else {
+    return 2019 - age;
+  }
 }
 
 const greeting1 = createGreeting('Nicole', 30);

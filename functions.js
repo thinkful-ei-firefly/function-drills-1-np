c5bac18a7a5f31adbdad6019d7b0a2d2ce95f932
Nicole Pasquale <NSquale212@gmail.com>
2019-07-08 15:29:48 -0500

    Fifth Commit

--- functions.js
+++ functions.js
@@ -1,8 +1,8 @@
 'use strict';
 
-function createGreeting() {
-  return 'Hi, my name is Nicole and I\'m 30 years old';
+function createGreeting(name, age) {
+  return `Hi, my name is ${name} and I'm ${age} years old`;
 }
 
-const greeting1 = createGreeting();
+const greeting1 = createGreeting('Nicole', 30);
 console.log(greeting1);
